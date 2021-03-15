781257487f25b644039a34dd82df4bad8c14873c
Bo Weber <bo.weber@tu-dortmund.de>
2021-03-15 11:18:35 +0100

    Updated glmer_fit

--- R/rpcm_fit.R
+++ R/rpcm_fit.R
@@ -29,34 +29,3 @@ rpcm_fit <- function(y,
     )
     return(rval)
 }
-
-glmer_fit <- function(y,
-                      x = NULL,
-                      start = NULL,
-                      weights = NULL,
-                      offset = NULL,
-                      ...,
-                      estfun = FALSE,
-                      object = FALSE) {
-    if (!(is.null(x) || NCOL(x) == 0L)) warning("x not used")
-    if (!is.null(offset)) warning("offset not used")
-
-    y_data_frame <- as.data.frame(y)
-    y_data_frame <- y_data_frame %>%
-        mutate(id = seq_len(nrow(y))) %>%
-        gather(-id, key = "item", value = "count")
-
-    glmer_result <- lme4::glmer(
-        count ~ 0 + item + (1 | id),
-        data = y_data_frame,
-        family = "poisson"
-    )
-
-    rval <- list(
-        coefficients = exp(lme4::fixef(glmer_result)),
-        objfun = summary(glmer_result)[[6]],
-        estfun = if (estfun) glmer_result@optinfo$derivs$gradient else NULL,
-        object = if (object) glmer_result else NULL
-    )
-    return(rval)
-}
