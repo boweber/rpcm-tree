394ad37a65074510c286fc0004e6e593dd60d6df
Bo Weber <bo.weber@tu-dortmund.de>
2021-04-23 15:02:12 +0200

    Fixed an rmse error

--- simulation/rpcm_tree_simulation+rmse.R
+++ simulation/rpcm_tree_simulation+rmse.R
@@ -57,17 +57,16 @@ glmer_item_estimates <- function(glmer_output, is_numeric) {
     estimates <- as.matrix(summary(glmer_effects)$effect)
 
     if (is_numeric) {
-        estimates <- t(estimates)
-        rownames(estimates) <- c("reference", "focal")
+        colnames(estimates) <- c("reference", "focal")
     }
 
     estimates <- estimates[
         ## sorts the items
-        , stringr::str_sort(colnames(estimates), numeric = TRUE)
+        stringr::str_sort(rownames(estimates), numeric = TRUE),
     ]
     estimates <- log(estimates)
     return(data.frame(
-        reference = estimates[which(rownames(estimates) == "reference"), ],
-        focal = estimates[which(rownames(estimates) == "focal"), ]
+        reference = estimates[, which(colnames(estimates) == "reference")],
+        focal = estimates[, which(colnames(estimates) == "focal")]
     ))
 }
