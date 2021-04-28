21b44fe1a6edbe3ee9d716cd222a0df4d8b0612f
Bo Weber <bo.weber@tu-dortmund.de>
2021-04-28 18:17:34 +0200

    Updated loading libraries

--- simulation/rpcm_tree_simulation+interpretation.R
+++ simulation/rpcm_tree_simulation+interpretation.R
@@ -2,4 +2,21 @@ load("Simulation_Study_I.RData")
 load("Simulation_Study_II.RData")
 
 simulation_1_results
-simulation_2_results
+
+simulation_1_column_names <- c(
+    "error_rate",
+    "mean_time",
+    "min_time",
+    "max_time",
+    "rmse",
+    "mean_ari"
+)
+rpcm_tree <- simulation_1_results[, c(1, 3, 5, 7, 9, 11)]
+lr_test <- simulation_1_results[, c(2, 4, 6, 8, 10, 12)]
+colnames(rpcm_tree) <- simulation_1_column_names
+colnames(lr_test) <- simulation_1_column_names
+
+
+simulation_1_list <- list(rpcm_tree, lr_test)
+
+table(rpcm_tree)
